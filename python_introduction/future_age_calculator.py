901a84a97e6bea325131f883577bfb3de58e7041
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-12 16:43:06 +0300

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #Asking the user for their current age
-#current_age = int ( input (“How old are you? ”) )
+current_age = int ( input (“How old are you? ”) )
 current_age = 30
 #Calulating the futer age
 age = current_age + 27
