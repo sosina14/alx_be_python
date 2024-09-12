3863db343cabf1ea8146fed1c933ae01d4733956
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-12 16:34:57 +0300

    Create future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#Asking the user for their current age
+current_age = int ( input (“How old are you? ”) )
+
+#Substructing the current year from 2025    
+reminder = 2050 - 2023
+
+#Calulating the futer age
+futer_age = current_age + reminder
+
+#printing the futer age
+print (f"In 2050, you will be {futer_age} years old.")
