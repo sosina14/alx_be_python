9929c88ae3291a6711c14ddb08128b2daae1e4b2
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-13 13:45:54 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 #User Input for Financial Details:
-income = float ( input ("Enter your monthly income:" ))
-expenses = float (input ( "Enter your total monthly expenses: " ))
+income = int ( input ("Enter your monthly income:" ))
+expenses = int (input ( "Enter your total monthly expenses: " ))
 
 #Calculate Monthly Savings:
-monthly_savings = float ( income - expenses )
+monthly_savings = int ( income - expenses )
 
 #Calulating projected savings:
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
