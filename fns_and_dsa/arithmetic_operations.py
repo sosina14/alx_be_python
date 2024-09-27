61e3689ad7db1baea530b8ac97ddaf9119579419
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-27 18:46:43 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,17 @@
-def perform_operation(num1, num2, operation):
+def perform_operation(num1,num2, operation ):
     match operation:
         case 'add':
-            return num1 + num2
-        case 'subtract':
+            return num1 + num2 
+        case 'subtract':  
             return num1 - num2
         case 'multiply':
             return num1 * num2
         case 'divide':
             if num2 == 0:
-                return 'Division by 0 is undefined'
-            elif num2 != 0:
-                return num1 / num2
-        case _:
+                return 'Number can't divide by zero'
+            else num2 != 0:
+                return num1/num2
+         case _:
             'Error! Unrecognized operation'
+  
+  
