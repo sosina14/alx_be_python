ceb1085a91c19128568902f9397e2f97b099e7a4
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-27 21:34:09 +0300

    Create shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,28 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def mainn():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            shopping_list.append(input ("Enter the name of the item : "))
+        elif choice == '2':
+            shopping_list.remove(input ("Enter the item to remove : "))
+        elif choice == '3':
+           print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+#if __name__ == "__main__":
+mainn()
+
