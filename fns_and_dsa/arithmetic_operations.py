cea46bf4313341b249686eedaaae96ce5507ef96
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-27 17:47:33 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,25 +1,14 @@
-def perform_operation(num1,num2, operation ):
- 
-    match operation:
-        case "add":
-            add = num1 + num2 
-            print(f"Result: {add}")
+def perform_operation(num1 , num2 , operation ):
     
-        case "subtract":
-            subtract = num1 - num2 
-            print(f"Result: {subtract}")
-    
-        case "multiply":
-            multiply = num1 * num2 
-            print(f"Result: {multiply}")
-        
-        case "divide":
-            divide = num1/num2
-            if num2 != 0:
-                print(f"Result: {divide}")
-               
-            else:
-                print("")
-    
-    return (num1, num2, operation)
-#perform_operation(num1, num2, operation)
+    if operation == "add" :
+        return num1 + num2
+    elif operation == "subtruct":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2 
+    elif operation == "divide":
+        if num2 == 0:
+           return "Number can't be divide by zero" 
+        return num1/num2
+    else:
+        return "Error input"  
