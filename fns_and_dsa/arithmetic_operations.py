2dc7060388692cfc74153d381fe475be7dd0801e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-09-27 18:47:49 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1,num2, operation ):
             return num1 * num2
         case 'divide':
             if num2 == 0:
-                return 'Number can't divide by zero'
+                return 'Division by 0 is undefined'
             else num2 != 0:
                 return num1/num2
          case _:
