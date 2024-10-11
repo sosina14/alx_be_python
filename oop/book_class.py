f414f09d27bff9237fd01e5d07e5d63c5b9ad507
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-12 00:15:05 +0300

    oop task1

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,6 +6,6 @@ class Book:
     def __del__(self):
         print(f"Deleting {self.title}")
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}."
+        return f"{self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
         return f"Book({self.title!r}, {self.author!r}, {self.year!r})"
