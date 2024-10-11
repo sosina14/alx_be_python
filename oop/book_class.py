93baa9cd67af6fc15fdcd1240b597bd9f1ca27a2
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-12 00:03:33 +0300

    oop task1

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,12 +1,11 @@
-from book_class import Book
 class Book:
     def __init__(self, title , author , year):
         self.title = title 
         self.author = author 
         self.year = year      
     def __del__(self):
-        Prints("Deleting title of the book upon object deletion.")
+        Print("Deleting title of the book upon object deletion.")
     def __str__(self):
-        return f"{self.title} by {self.author}, {self.pages} pages."
+        return f"{self.title} by {self.author}, published in {self.year}."
     def __repr__(self):
         return f"Book({self.title!r}, {self.author!r}, {self.year!r})"
