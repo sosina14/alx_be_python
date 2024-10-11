0c7e619b75f20b84d1b02c5a518bc9756c5596d9
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-11 23:44:56 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,11 @@
-# book_class.py:
 class Book:
     def __init__(self, title , author , year):
         self.title = title 
         self.author = author 
-        self.year = year
-        
+        self.year = year      
     def __del__(self):
-        Prints ("Deleting title of the book upon object deletion.")
-         
-"""String Representation (__str__): Returns a string in the format "(title) by (author), published in (year)"."""
-
+        Prints("Deleting title of the book upon object deletion.")
     def __str__(self):
-        return f"'{self.title}' by {self.author}, {self.pages} pages."
-        
-"""Official Representation (__repr__): Returns a string that would recreate the Book instance: f"Book('{self.title}', '{self.author}', {self.year})"""
-
+        return f"{self.title} by {self.author}, {self.pages} pages."
     def __repr__(self):
         return f"Book('{self.title!r}', '{self.author!r}', {self.year!r})
