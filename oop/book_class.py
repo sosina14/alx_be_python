3da268fe2e7419502b284b89036588eb07b7e193
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-12 00:05:14 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,10 +1,10 @@
 class Book:
-    def __init__(self, title , author , year):
+    def __init__(self, title, author, year):
         self.title = title 
         self.author = author 
         self.year = year      
     def __del__(self):
-        Print("Deleting title of the book upon object deletion.")
+        print(f"Deleting {self.title}")
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}."
     def __repr__(self):
