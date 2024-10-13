7c6bb2d85495bd61459fe315a36be9e0597413ee
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-13 23:26:48 +0300

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,14 +1,14 @@
 import math
 
-class Shap:
+class Shape:
     def __init__(self, length , width):
         self.length = length
         self.width = width
     def area(self):
         raise NotImplementedError("error")
-class Rectangle(Shap):
+class Rectangle(Shape):
     def __init__(self, length , width):
         print(f"Area of the rectangle is {length * width} square")
-class Circle(Shap):
+class Circle(Shape):
     def __init__(self, radis):
         print(f"Area of the circule is {math.pi*radis*radis}")
